e9311e94c2b16f926df1d82839a574d9a30a035a
cyber-lerokaid <rsantoss0012@gmail.com>
2026-05-23 23:31:02 -0300

    feat: add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import { ProtectedRoute } from './components/auth/ProtectedRoute';
 import Dashboard from './pages/Dashboard';
 import PublicOSLink from './pages/PublicOSLink';
@@ -30,6 +31,7 @@ function App() {
         <Route path="/os/:osNumber" element={<PublicOSLink />} />
         <Route path="/checkin" element={<ProtectedRoute><CheckinWizard /></ProtectedRoute>} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
